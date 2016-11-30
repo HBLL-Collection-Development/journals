abb003639fed93e5547c4e625ad18b76fdcb2a73
Jared Howland <git@jaredhowland.com>
2016-11-30 11:57:55 -0700

    [ADD] Total spend to fund code statistics

--- fund.php
+++ fund.php
@@ -23,6 +23,11 @@ $db->query('SELECT ROUND(AVG(CASE WHEN `price2016` > `price2015` THEN `price2016
 $db->bind('fund', $fund);
 $averagePrice = $db->single();
 $averagePrice = $averagePrice['averagePrice'];
+// Total spend
+$db->query('SELECT SUM(CASE WHEN `price2016` > `price2015` THEN `price2016` ELSE `price2015` END) AS `price` FROM `journals` WHERE `subAccount` = :fund AND ((`price2016` IS NOT NULL AND `price2016` > 0) OR (`price2015` IS NOT NULL AND `price2015` > 0)) GROUP BY `subAccount`');
+$db->bind('fund', $fund);
+$totalSpend = $db->single();
+$totalSpend = $totalSpend['price'];
 // Average use
 $db->query('SELECT ROUND(AVG(`usage2015`), 0) AS `averageUse` FROM `journals` WHERE `subAccount` = :fund AND `usage2015` IS NOT NULL');
 $db->bind('fund', $fund);
@@ -40,4 +45,4 @@ $averageIpp = $db->single();
 $averageIpp = $averageIpp['averageIpp'];
 $db = null;
 
-new \Journals\Twig('fund.tmpl', array('fund' => $fund, 'fundName' => $fundName, 'librarians' => $librarians, 'numTitles' => $numTitles, 'averagePrice' => $averagePrice, 'averageUse' => $averageUse, 'averageHIndex' => $averageHIndex, 'averageIpp' => $averageIpp, 'journals' => $journals));
+new \Journals\Twig('fund.tmpl', array('fund' => $fund, 'fundName' => $fundName, 'librarians' => $librarians, 'numTitles' => $numTitles, 'totalSpend' => $totalSpend, 'averagePrice' => $averagePrice, 'averageUse' => $averageUse, 'averageHIndex' => $averageHIndex, 'averageIpp' => $averageIpp, 'journals' => $journals));
