57cdf77fc2b9a7be0dbf833d1f65d53c31b13979
Jared Howland <git@jaredhowland.com>
2016-12-02 11:27:10 -0700

    [ADD] Detailed usage to journal details pages

--- journal.php
+++ journal.php
@@ -10,6 +10,21 @@ $db = new Database\Database($configs['database']['host'], $configs['database']['
 $db->query('SELECT * FROM `journals` WHERE `titleControl` = :titleControl GROUP BY `titleControl`');
 $db->bind('titleControl', $titleControl);
 $journal = $db->single();
+
+$issn = $journal['issn'];
+
+$db->query('SELECT `total`, `year`, `platform` FROM `USAGE` WHERE `issn` = :issn OR `eIssn` = :issn ORDER BY `platform`, `year`');
+$db->bind('issn', $issn);
+$usage = $db->resultSet();
+
+$db->query('SELECT MIN(`year`) AS `min`, MAX(`year`) AS `max` FROM `USAGE` WHERE `issn` = :issn OR `eIssn` = :issn');
+$db->bind('issn', $issn);
+$range = $db->single();
+
+$db->query('SELECT SUM(`total`) AS `total`, `year` FROM `usage` WHERE `issn` = :issn OR `eIssn` = :issn GROUP BY `year` ORDER BY `YEAR`');
+$db->bind('issn', $issn);
+$totals = $db->resultSet();
+
 $db = null;
 
-new \Journals\Twig('journal.tmpl', array('titleControl' => $titleControl, 'journal' => $journal));
+new \Journals\Twig('journal.tmpl', array('titleControl' => $titleControl, 'usage' => $usage, 'totals' => $totals, 'range' => $range, 'journal' => $journal));
